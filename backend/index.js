37a25135777708b2a4dae44f7699a09eef0a687d
vani <vanibodasingu.com>
2025-12-04 21:03:08 +0530

    bla

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-"http://localhost:5173",
+const allowedOrigins = [
+  "http://localhost:5173",
   "https://jobsfor-u.vercel.app",
   "https://jobsfor-u-4qa6.vercel.app",
   "https://jobs4u-ai.vercel.app",
